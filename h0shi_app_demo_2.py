c183801fd482e5d7f70a41abb404bc588efe814d
hamihoshiyama <139207081+hamihoshiyama@users.noreply.github.com>
2024-07-30 12:12:04 +0900

    Update h0shi_app_demo_2.py

--- h0shi_app_demo_2.py
+++ h0shi_app_demo_2.py
@@ -322,8 +322,8 @@ else:
                         response = openai.fine_tuning.jobs.retrieve(finetuning_id)
                         model_name = response.fine_tuned_model
                         model_names.append(model_name)
+                        save_model(st.session_state.username, model_name)
                     st.session_state.model_names = model_names
-                    st.session_state.all_succeeded = True
                     st.session_state.all_succeeded = True
                     st.success("Fine-tuning succeeded!")
                     st.rerun()
